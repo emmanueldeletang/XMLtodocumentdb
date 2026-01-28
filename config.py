f5deba9e273277530789ed753342db00fbb28d24
Emmanuel DELETANG <t_emdel@hotmail.com>
2026-01-28 16:50:15 +0100

    Create config.py for MongoDB Loader and Exporter
    Add configuration for MongoDB connection settings

---
+++ config.py
@@ -0,0 +1,7 @@
+"""
+Configuration file for XML to MongoDB Loader & Exporter
+"""
+
+# MongoDB (Cosmos DB for MongoDB API) configuration
+DEFAULT_CONNECTION_STRING = "mongodb+srv://user:Tpassword@your-mongo.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+DEFAULT_DATABASE = "your"
